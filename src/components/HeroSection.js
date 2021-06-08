51f81ded71cc539382828fba0eb3324bb57a5a4e
Azizul Bappy <azizulbappy6@gmail.com>
2021-06-09 01:15:27 +0600

    fix hero section

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -6,7 +6,7 @@ function HeroSection() {
   return (
     <div className="heroSection container-fluid z-10 pt-20">
       <div className="row justify-content-center">
-        <div className="col-11 col-md-6 offset-1 pt-3 pt-md-5">
+        <div className="col-11 col-md-6 offset-1 pt-3 pt-md-5 self-center">
           <h1>Building exactly the eCommerce website you want.</h1>
           <p>
             WooCommerce is a customizable, open-source eCommerce platform built
